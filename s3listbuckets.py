64cb534fc762a1f2d2a5777135ce88bf98e3750b
Johan Swensson <kupo@kupo.se>
2015-01-05 13:11:09 +0100

    update list script

--- s3listbuckets.py
+++ s3listbuckets.py
@@ -10,8 +10,8 @@ files=0
 buckets = conn.get_all_buckets()
 
 for i in buckets:
+  print i.name
   for j in conn.get_bucket(i):
-    #print j.name + ': ' + str(j.size/1024/1024) +'mb.'
     size+=j.size
     files+=1
 
